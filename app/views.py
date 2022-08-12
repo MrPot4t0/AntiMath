55b6cc9924034f5c81620a9320f1aa606f7d210a
MrPot4t0 <MrPot4t0@github.com>
2022-08-12 10:27:26 -0400

    login and style update

--- app/views.py
+++ app/views.py
@@ -5,12 +5,16 @@ from flask import render_template, request
 def index():    
     return render_template("public/index.html")
 
-@app.route("/about")
+@app.route("/profile")
 def about():
-    return render_template("public/about.html") 
-    
+    return render_template("public/about.html")
+
+@app.route("/singup")
+def singup():
+    return render_template("public/singup.html")
+
 @app.route("/login")
-def sign_up():
+def login():
     return render_template("public/login.html")
 
 
